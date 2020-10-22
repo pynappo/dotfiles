4aaac2d9e2098c09accf6974e153a04c9b00818c
slow <73202350+slow@users.noreply.github.com>
2020-10-22 01:37:17 +0100

    release

---
+++ README.md
@@ -0,0 +1,15 @@
+# Double Click Voice Channels [![CodeFactor](https://www.codefactor.io/repository/github/slow/double-click-vc/badge)](https://www.codefactor.io/repository/github/slow/double-click-vc) [![GitHub issues](https://img.shields.io/github/issues/slow/double-click-vc?style=flat)](https://github.com/slow/double-click-vc/issues) [![GitHub stars](https://img.shields.io/github/stars/slow/double-click-vc?style=flat)](https://github.com/slow/double-click-vc/stargazers)
+
+A powercord plugin that requires you to double click voice channels to join them.
+
+# Requirements
+
+-  Powercord
+
+# Installation
+
+-  `git clone https://github.com/slow/double-click-vc`
+
+# Support
+
+-  eternal#1000
---
+++ index.js
@@ -0,0 +1,36 @@
+const { Plugin } = require('powercord/entities');
+const { inject, uninject } = require('powercord/injector');
+const { getModuleByDisplayName } = require('powercord/webpack');
+
+module.exports = class DoubleClickVc extends Plugin {
+   async startPlugin() {
+      const ChannelItem = getModuleByDisplayName('ChannelItem', false);
+      inject('double-click-vc', ChannelItem.prototype, 'render', (args, res) => {
+         const channel = this.getNestedProp(res, 'props.children.1.props.children.1.props.children.1.props.channel');
+         if (channel && channel.type == 2) {
+            const props = this.getNestedProp(res, 'props.children.1.props.children.0.props');
+            if (props) {
+               const onClick = props.onClick;
+               props.onDoubleClick = onClick;
+               props.onClick = () => {};
+            } else {
+               this.log('Failed to get nested props.');
+            }
+         } else if (!channel) {
+            this.log('Failed to determine channel type.');
+         }
+         
+         return res
+      });
+   }
+
+   pluginWillUnload() {
+      uninject('double-click-vc');
+   }
+
+   getNestedProp(obj, path) {
+      return path.split('.').reduce(function (obj, prop) {
+         return obj && obj[prop];
+      }, obj);
+   }
+};
