3b504528c63b721fcf1e80b72bd97eca4a5efdbc
AAGaming <aa@bigdumb.gq>
2020-11-09 06:45:22 -0500

    better method of loading settings into the modal, using refs (#7)

--- components/PluginSettings.jsx
+++ components/PluginSettings.jsx
@@ -10,6 +10,13 @@ const FormTitle = getModuleByDisplayName('FormTitle', false)
 module.exports = class PluginSettings extends React.Component {
   constructor (props) {
     super(props)
+    this.settingsRef = el => {
+      const { plugin } = this.props
+      if(!el) return
+      const panel = plugin.getSettingsPanel()
+      if(typeof panel == 'string') el.innerHTML = panel
+      else el.append(panel)
+    }
   }
 
   render () {
@@ -22,20 +29,9 @@ module.exports = class PluginSettings extends React.Component {
           <Modal.CloseButton onClick={closeModal}/>
         </Modal.Header>
         <Modal.Content>
-          <div class='plugin-settings' id={'plugin-settings-' + plugin.getName()}></div>
+          <div class='plugin-settings' ref={this.settingsRef} id={'plugin-settings-' + plugin.getName()}></div>
         </Modal.Content>
       </Modal>
     )
-  }
-
-  componentDidMount () {
-    setTimeout(() => {
-      const { plugin } = this.props
-      const el = document.getElementById(`plugin-settings-${plugin.getName()}`)
-      if(!el) return
-      const panel = plugin.getSettingsPanel()
-      if(typeof panel == 'string') el.innerHTML = panel
-      else el.append(panel)
-    })
   }
 }
