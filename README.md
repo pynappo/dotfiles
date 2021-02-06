1be9a0e2213feeda187cc46ffdc2dee6dd34aebb
eternal <73202350+slow@users.noreply.github.com>
2021-02-06 22:57:31 +0000

    release

---
+++ README.md
@@ -0,0 +1,11 @@
+# Profile Picture Link [![CodeFactor](https://www.codefactor.io/repository/github/slow/profile-picture-link/badge)](https://www.codefactor.io/repository/github/slow/profile-picture-link) [![GitHub issues](https://img.shields.io/github/issues/slow/profile-picture-link?style=flat)](https://github.com/slow/profile-picture-link/issues) [![GitHub stars](https://img.shields.io/github/stars/slow/profile-picture-link?style=flat)](https://github.com/slow/profile-picture-link/stargazers)
+
+Allows you to click people's profile pictures in their user modal and open them in your browser.
+
+# Requirements
+
+-  Powercord
+
+# Installation
+
+-  `git clone https://github.com/slow/profile-picture-link`
---
+++ index.js
@@ -0,0 +1,27 @@
+const { Plugin } = require('powercord/entities');
+const { getModule } = require('powercord/webpack');
+const { AnimatedAvatar } = getModule(['AnimatedAvatar'], false);
+
+module.exports = class ProfilePictureLink extends Plugin {
+   startPlugin() {
+      this.original = AnimatedAvatar.type;
+      AnimatedAvatar.type = function (props) {
+         try {
+            const node = AnimatedAvatar(props).type(props);
+            node.props.onClick = v => {
+               if (v.target.parentElement.classList.contains("header-QKLPzZ")) {
+                  window.open(props.src.replace(/(?:\?size=\d{3,4})?$/, "?size=4096"), "_blank");
+               }
+            };
+            return node;
+         } catch { }
+         return React.createElement(AnimatedAvatar, props);
+      };
+
+      this.cancel = () => AnimatedAvatar.type = this.original;
+   }
+
+   pluginWillUnload() {
+      this.cancel && this.cancel();
+   }
+};
