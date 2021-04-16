a72c6e1f77d2f5836a97e09de44dfa86961c2805
eternal <73202350+slow@users.noreply.github.com>
2021-04-16 02:02:01 +0100

    add pointer cursor if pfp is clickable

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ const { AnimatedAvatar } = getModule(['AnimatedAvatar'], false);
 
 module.exports = class ProfilePictureLink extends Plugin {
    startPlugin() {
+      this.loadStylesheet('style.css')
       this.original = AnimatedAvatar.type;
       AnimatedAvatar.type = (props) => {
          try {
             const node = this.original(props).type(props);
+            const className = node.props.className;
+            if(className && className.includes('avatar-3EQepX')) {
+               node.props.className += " picture-link";
+            }
+
             node.props.onClick = v => {
                if (v.target.parentElement.classList.contains("header-QKLPzZ")) {
                   window.open(props.src.replace(/(?:\?size=\d{3,4})?$/, "?size=4096"), "_blank");
                }
             };
+            
             return node;
          } catch { }
          return React.createElement(this.original, props);
