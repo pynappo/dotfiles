09c01dfe3fb8e04657af4feff6e90c94610ca57e
eternal <73202350+slow@users.noreply.github.com>
2021-05-04 18:32:16 +0100

    fix some padding

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const { AnimatedAvatar } = getModule(['AnimatedAvatar'], false);
 
 module.exports = class ProfilePictureLink extends Plugin {
    startPlugin() {
-      this.loadStylesheet('style.css')
+      this.loadStylesheet('style.css');
       this.original = AnimatedAvatar.type;
       AnimatedAvatar.type = (props) => {
          try {
             const node = this.original(props).type(props);
             const className = node.props.className;
-            if(className && className.includes('avatar-3EQepX')) {
+            if (className && className.includes('avatar-3EQepX')) {
                node.props.className += " picture-link";
             }
 
@@ -19,7 +19,7 @@ module.exports = class ProfilePictureLink extends Plugin {
                   window.open(props.src.replace(/(?:\?size=\d{3,4})?$/, "?size=4096"), "_blank");
                }
             };
-            
+
             return node;
          } catch { }
          return React.createElement(this.original, props);
