043d4e19189767fa910f0a6c4352764146cee791
eternal <73202350+slow@users.noreply.github.com>
2021-06-08 06:08:57 +0100

    make this not break every update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Plugin } = require('powercord/entities');
 const { React, getModule } = require('powercord/webpack');
 const { AnimatedAvatar } = getModule(['AnimatedAvatar'], false);
+const { header, avatar } = getModule(['avatar', 'header', 'nameTag'], false);
 
 module.exports = class ProfilePictureLink extends Plugin {
    startPlugin() {
@@ -10,13 +11,13 @@ module.exports = class ProfilePictureLink extends Plugin {
          try {
             const node = this.original(props).type(props);
             const className = node.props.className;
-            if (className && className.includes('avatar-3EQepX')) {
-               node.props.className += " picture-link";
+            if (className && className.includes(avatar)) {
+               node.props.className += ' picture-link';
             }
 
             node.props.onClick = v => {
-               if (v.target.parentElement.classList.contains("header-QKLPzZ")) {
-                  window.open(props.src.replace(/(?:\?size=\d{3,4})?$/, "?size=4096"), "_blank");
+               if (v.target.parentElement.classList.contains(header)) {
+                  window.open(props.src.replace(/(?:\?size=\d{3,4})?$/, '?size=4096'), '_blank');
                }
             };
 
