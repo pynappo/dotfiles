628fe4cb507ddcca1a9e6a4e2056b1d09d0a67c6
eternal <eternal.git@gmail.com>
2022-02-21 07:54:15 +0000

    fix my beloved

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const { React, getModule, contextMenu: { openContextMenu, closeContextMenu } } = require('powercord/webpack');
 const { inject, uninject } = require('powercord/injector');
 const { findInReactTree } = require('powercord/util');
+const { Menu } = require('powercord/components');
 const { Plugin } = require('powercord/entities');
 const { clipboard } = require('electron');
-
-const ProfileModalHeader = getModule(m => m.default?.displayName == 'UserProfileModalHeader', false);
-const Banner = getModule(m => m.default?.displayName == 'UserBanner', false);
-const ContextMenu = getModule(['MenuGroup', 'MenuItem'], false);
-const classes = getModule(['discriminator', 'header'], false);
-const Banners = getModule(['getUserBannerURL'], false);
 
 const SizeRegex = /(?:\?size=\d{3,4})?$/;
 
 module.exports = class PictureLink extends Plugin {
    startPlugin() {
       this.loadStylesheet('style.css');
+      this.promises = { cancelled: false };
+
+      this.patchAvatars();
+      this.patchBanners();
+   }
+
+   async patchAvatars() {
+      const ProfileModalHeader = await this.findLazy(m => m.default?.displayName == 'UserProfileModalHeader');
+      const classes = getModule(['relationshipButtons'], false);
+      if (this.promises.cancelled) return;
+
       inject('pfp-link-profile', ProfileModalHeader, 'default', (args, res) => {
          const avatar = findInReactTree(res, m => m?.props?.className == classes.avatar);
          const image = args[0].user?.getAvatarURL?.(false, 4096, true)?.replace('.webp', '.png');
@@ -23,8 +29,8 @@ module.exports = class PictureLink extends Plugin {
             avatar.props.onClick = () => open(image);
 
             avatar.props.onContextMenu = (e) => openContextMenu(e, () =>
-               React.createElement(ContextMenu.default, { onClose: closeContextMenu },
-                  React.createElement(ContextMenu.MenuItem, {
+               React.createElement(Menu.Menu, { onClose: closeContextMenu },
+                  React.createElement(Menu.MenuItem, {
                      label: 'Copy Avatar URL',
                      id: 'copy-avatar-url',
                      action: () => clipboard.writeText(image)
@@ -35,6 +41,13 @@ module.exports = class PictureLink extends Plugin {
 
          return res;
       });
+
+      ProfileModalHeader.default.displayName = 'UserProfileModalHeader';
+   }
+
+   async patchBanners() {
+      const Banner = getModule(m => m.default?.displayName == 'UserBanner', false);
+      const Banners = getModule(['getUserBannerURL'], false);
 
       inject('pfp-link-banner', Banner, 'default', (args, res) => {
          const handler = findInReactTree(res.props.children, p => p?.onClick);
@@ -49,8 +62,8 @@ module.exports = class PictureLink extends Plugin {
             };
 
             res.props.onContextMenu = (e) => openContextMenu(e, () =>
-               React.createElement(ContextMenu.default, { onClose: closeContextMenu },
-                  React.createElement(ContextMenu.MenuItem, {
+               React.createElement(Menu.Menu, { onClose: closeContextMenu },
+                  React.createElement(Menu.MenuItem, {
                      label: 'Copy Banner URL',
                      id: 'copy-banner-url',
                      action: () => clipboard.writeText(image)
@@ -67,7 +80,48 @@ module.exports = class PictureLink extends Plugin {
       Banner.default.displayName = 'UserBanner';
    }
 
+   findLazy(filter) {
+      const direct = getModule(filter, false);
+      if (direct) return direct;
+
+      const oldPush = window.webpackChunkdiscord_app.push;
+
+      return new Promise(resolve => {
+         Object.defineProperty(window.webpackChunkdiscord_app, 'push', {
+            configurable: true,
+            writable: true,
+            value: (chunk) => {
+               const [, modules] = chunk;
+
+               for (const id in modules) {
+                  const orig = modules[id];
+
+                  modules[id] = (module, exports, require) => {
+                     Reflect.apply(orig, null, [module, exports, require]);
+
+                     try {
+                        const res = filter(exports);
+
+                        if (res) {
+                           window.webpackChunkdiscord_app.push = oldPush;
+                           resolve(exports);
+                        }
+                     } catch { }
+                  };
+
+                  Object.assign(modules[id], orig, {
+                     toString: () => orig.toString()
+                  });
+               }
+
+               return Reflect.apply(oldPush, window.webpackChunkdiscord_app, [chunk]);
+            }
+         });
+      });
+   }
+
    pluginWillUnload() {
+      this.promises.cancelled = false;
       uninject('pfp-link-banner');
       uninject('pfp-link-profile');
    }
