e3f3fc380845615f4ad3d1c366fc1f34ddf95118
pynappo <43484729+pynappo@users.noreply.github.com>
2022-03-20 23:07:23 -0700

    Added powershell profile

---
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,2 @@
+oh-my-posh --init --shell pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/v$(oh-my-posh --version)/themes/jandedobbeleer.omp.json | Invoke-Expression
+Function dotfiles { git --git-dir=$HOME/.files/ --work-tree=$HOME @Args }
