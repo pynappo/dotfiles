6dc7aa63e817ec3d70bb15093d3d9f79058a1ea2
pynappo <43484729+pynappo@users.noreply.github.com>
2022-03-22 00:43:44 -0700

    Updated pkgs and added pacup

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,6 @@
 oh-my-posh --init --shell pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/v$(oh-my-posh --version)/themes/jandedobbeleer.omp.json | Invoke-Expression
 Function dotfiles { git --git-dir=$HOME/.files/ --work-tree=$HOME @Args }
+Function pacup {
+	winget export -o $HOME/.files/winget.txt --accept-source-agreements
+	scoop export > $HOME/.files/scoop.txt
+}
