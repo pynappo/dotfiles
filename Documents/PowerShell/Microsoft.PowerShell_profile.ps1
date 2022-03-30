a2bb98b2174c1e8222b67b89860b761420f8615a
David <git+lehtien.david@gmail.com>
2022-03-30 14:33:52 -0700

    Combined scoopimport into one bucket

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,19 @@
 oh-my-posh --init --shell pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/v$(oh-my-posh --version)/themes/jandedobbeleer.omp.json | Invoke-Expression
+
 Function dotfiles { git --git-dir=$HOME/.files/ --work-tree=$HOME @Args }
 set-alias -name df -value dotfiles
 
-Function pacup {
-	winget export -o $Home/.files/winget.txt --accept-source-agreements
-	scoop export > $Home/.files/scoop.txt
-	scoop bucket list | Export-Csv -Path $Home\.files\scoop-buckets.csv
+Function pacup ([string]$Path = "$Home\.files\"){
+	winget export -o ($Path + 'winget.txt') --accept-source-agreements
+	scoop export > ($Path + 'scoop.txt')
+	scoop bucket list | Export-Csv -Path ($Path + 'scoop-buckets.csv')
 }
-Function scoopImportBuckets { Import-Csv -Path @Args | ForEach-Object{ scoop bucket add $_.Name $_.Source } }
-Function scoopImport { gc @Args | ForEach-Object { scoop install ($_ -split " ")[0] } }
+
+Function scoopImport { 
+	Param ([string]$b)
+	if ($b) { Import-Csv -Path $b | ForEach-Object { scoop bucket add $_.Name $_.Source } } 
+	else {"No bucket list supplied, continuing..."}
+	Param ([string]$p)
+	if ($p) { gc $p | ForEach-Object { scoop install ($_ -split " ")[0] } } # -p <path to package list from scoop export> 
+      else {"No package list supplied, ending..."}
+}
