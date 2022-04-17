fababc99749f6ffe5db4ba9eafeac9627b6f5f31
pynappo <git+lehtien.david@gmail.com>
2022-04-17 03:48:36 -0700

    :wrench: Updated apps and added console history

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,19 +1,27 @@
-oh-my-posh --init --shell pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/v$(oh-my-posh --version)/themes/jandedobbeleer.omp.json | Invoke-Expression
+oh-my-posh init pwsh --config $Home/.files/.global/pynappo.omp.json | Invoke-Expression
+Enable-PoshTooltips
+Enable-PoshLineError
 
-Function dotfiles { git --git-dir=$HOME/.files/ --work-tree=$HOME @Args }
+Import-Module posh-git
+$env:POSH_GIT_ENABLED = $true
+
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+Function Dotfiles { git --git-dir=$Home/.files/ --work-tree=$HOME @Args }
 set-alias -name df -value dotfiles
 
-Function pacup ([string]$Path = "$Home\.files\"){
+Function Pacup ([string]$Path = "$Home\.files\"){
 	winget export -o ($Path + 'winget.txt') --accept-source-agreements
 	scoop export > ($Path + 'scoop.txt')
 	scoop bucket list | Export-Csv -Path ($Path + 'scoop-buckets.csv')
 }
 
-Function scoopImport { 
-	Param ([string]$b)
-	if ($b) { Import-Csv -Path $b | ForEach-Object { scoop bucket add $_.Name $_.Source } } 
+Function ScoopImport { 
+	Param ([string]$Buckets)
+	if ($Buckets) { Import-Csv -Path $Buckets | ForEach-Object { scoop bucket add $_.Name $_.Source } } 
 	else {"No bucket list supplied, continuing..."}
-	Param ([string]$p)
-	if ($p) { gc $p | ForEach-Object { scoop install ($_ -split " ")[0] } } # -p <path to package list from scoop export> 
-      else {"No package list supplied, ending..."}
+	Param ([string]$Apps)
+	if ($Apps) { Get-Content $Apps | ForEach-Object { scoop install ($_ -split " ")[0] } }
+    else {"No apps list supplied, ending..."}
 }
