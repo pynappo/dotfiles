ce329361492d42828227aa44f14f12781b39404a
pynappo <43484729+pynappo@users.noreply.github.com>
2022-04-25 01:17:12 -0700

    Added scoop import script, general updates :sparkles:

--- BetterDiscord/plugins/VoiceActivity.plugin.js
+++ BetterDiscord/plugins/VoiceActivity.plugin.js
@@ -1,35 +1,12 @@
 /**
  * @name VoiceActivity
  * @author Neodymium
- * @version 1.1.4
+ * @version 1.1.6
  * @description Shows icons on the member list and info in User Popouts when somemone is in a voice channel.
  * @source https://github.com/Neodymium7/BetterDiscordStuff/blob/main/VoiceActivity/VoiceActivity.plugin.js
  * @updateUrl https://raw.githubusercontent.com/Neodymium7/BetterDiscordStuff/main/VoiceActivity/VoiceActivity.plugin.js
  * @invite fRbsqH87Av
  */
-/*@cc_on
-@if (@_jscript)
-    
-    // Offer to self-install for clueless users that try to run this directly.
-    var shell = WScript.CreateObject("WScript.Shell");
-    var fs = new ActiveXObject("Scripting.FileSystemObject");
-    var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
-    var pathSelf = WScript.ScriptFullName;
-    // Put the user at ease by addressing them in the first person
-    shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
-    if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
-        shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
-    } else if (!fs.FolderExists(pathPlugins)) {
-        shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
-    } else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
-        fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
-        // Show the user where to put plugins in the future
-        shell.Exec("explorer " + pathPlugins);
-        shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
-    }
-    WScript.Quit();
-
-@else@*/
 
 module.exports = (() => {
     const config = {
@@ -40,23 +17,18 @@ module.exports = (() => {
                     "name": "Neodymium"
                 }
             ],
-            "version": "1.1.4",
+            "version": "1.1.6",
             "description": "Shows icons on the member list and info in User Popouts when somemone is in a voice channel.",
             "github": "https://github.com/Neodymium7/BetterDiscordStuff/blob/main/VoiceActivity/VoiceActivity.plugin.js",
             "github_raw": "https://raw.githubusercontent.com/Neodymium7/BetterDiscordStuff/main/VoiceActivity/VoiceActivity.plugin.js"
         },
         "changelog": [
-            {"title": "Fixed", "type": "fixed", "items": ["Fixed plugin not being able to start"]},
-        ],
-        "main": "index.js"
+            {"title": "Fixed", "type": "fixed", "items": ["Fixed icons not showing on Discord startup"]},
+        ]
     };
 
     return !global.ZeresPluginLibrary ? class {
         constructor() { this._config = config; }
-        getName() { return config.info.name; }
-        getAuthor() { return config.info.authors.map(a => a.name).join(", "); }
-        getDescription() { return config.info.description; }
-        getVersion() { return config.info.version; }
         load() {
             BdApi.showConfirmationModal("Library Missing", `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {
                 confirmText: "Download Now",
@@ -74,7 +46,7 @@ module.exports = (() => {
     } : (([Plugin, Api]) => {
         const plugin = (Plugin, Library) => {
 
-            const { DiscordModules, DiscordSelectors, ContextMenu, ReactComponents, Settings } = Library;
+            const { DiscordModules, DiscordSelectors, ContextMenu, ReactComponents, ReactTools, Settings } = Library;
             const { ChannelStore, SelectedChannelStore, ChannelActions, GuildStore, GuildActions, NavigationUtils, UserStore, React, Permissions, DiscordPermissions } = DiscordModules;
             const { SettingPanel, Switch } = Settings;
             const VoiceStateStore = BdApi.findModuleByProps("getVoiceStateForUser");
@@ -86,6 +58,7 @@ module.exports = (() => {
             const People = BdApi.findModuleByDisplayName("People");
             const CallJoin = BdApi.findModuleByDisplayName("CallJoin");
             const Stage = BdApi.findModuleByDisplayName("Stage");
+            const memberListItem = `${DiscordSelectors.MemberList.members} > div > div:not(:first-child)`;
 
             const getSHCBlacklist = BdApi.Plugins.get("ShowHiddenChannels")?.exports.prototype.getBlackList?.bind(BdApi.Plugins.get("ShowHiddenChannels"));
 
@@ -508,6 +481,7 @@ module.exports = (() => {
                     BdApi.Patcher.after("VoiceActivity", MemberListItem.component.prototype, "render", (thisObject, _, ret) => {
                         if (thisObject.props.user) ret.props.children = React.createElement(VoiceIcon, {userId: thisObject.props.user.id, settings: this.settings});
                     });
+                    document.querySelectorAll(memberListItem).forEach(node => ReactTools.getReactInstance(node).return.return.return.return.stateNode.forceUpdate());
                 }
 
                 patchUserPopoutBody() {
@@ -562,6 +536,7 @@ module.exports = (() => {
                 onStop() {
                     BdApi.Patcher.unpatchAll("VoiceActivity");
                     VoiceStateStore.removeChangeListener(this.updateItems);
+                    document.querySelectorAll(memberListItem).forEach(node => ReactTools.getReactInstance(node).return.return.return.return.stateNode.forceUpdate());
                     BdApi.clearCSS("VoiceActivity");
                 }
 
@@ -569,6 +544,7 @@ module.exports = (() => {
                     return SettingPanel.build(() => {
                         this.saveSettings();
                         BdApi.Patcher.unpatchAll("VoiceActivity");
+                        document.querySelectorAll(memberListItem).forEach(node => ReactTools.getReactInstance(node).return.return.return.return.stateNode.forceUpdate());
                         this.patchUserPopoutBody();
                         if (this.settings.showMemberListIcons) this.patchMemberListItem();
                         if (this.settings.ignore) this.patchContextMenu();
