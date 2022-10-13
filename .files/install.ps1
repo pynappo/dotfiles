c37f386d82d71a212f75b2c80ed4f8a5341b6d3a
pynappo <43484729+pynappo@users.noreply.github.com>
2022-10-13 00:09:04 -0700

    Update install.ps1
    some more install commands

--- .files/install.ps1
+++ .files/install.ps1
@@ -1,3 +1,5 @@
+
+
 function Reload-Path {
   $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 }
@@ -10,22 +12,37 @@ irm get.scoop.sh | iex
 Reload-Path
 
 scoop install gsudo
+Reload-Path
 "Disabling UAC temporarily"
-Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0
-
+gsudo Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0
 
 "Setting up SSH"
-Remove-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
+gsudo {Remove-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0}
 winget install Microsoft.OpenSSH.Beta --override ADDLOCAL=Client
-gsudo [Environment]::SetEnvironmentVariable("Path", $env:Path + ';' + ${Env:ProgramFiles} + '\OpenSSH', [System.EnvironmentVariableTarget]::Machine)
-gsudo Set-Service -StartupType Automatic
-Start-Service ssh-agent
+gsudo {[Environment]::SetEnvironmentVariable("Path", $env:Path + ';' + ${Env:ProgramFiles} + '\OpenSSH', [System.EnvironmentVariableTarget]::Machine)
+Get-Service
+Set-Service -StartupType Automatic -Name ssh-agent
+Start-Service ssh-agent}
 "SSH done"
 
-git clone --bare https://github.com/pynappo/dotwindows.git .dotwindows.git
 git clone --bare https://github.com/pynappo/dotfiles.git .dotfiles.git
+git clone --bare https://github.com/pynappo/dotwindows.git .dotwindows.git
 
-Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0
+git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME checkout --force
+git --git-dir=$HOME/.dotwindows.git/ --work-tree=$HOME checkout --force
 
+scoop import .\.files\scoop.json
+winget import .\.files\winget.json
+
+Reload-Path
+
+. $PROFILE
+
+"Removing notepad"
+winget uninstall 9MSMLRH6LZF3
+gsudo {Remove-WindowsCapability -Online -Name Microsoft.Windows.Notepad.System~~~~0.0.1.0}
+
+"Re-enabling UAC"
+Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 1
 
 pwsh
