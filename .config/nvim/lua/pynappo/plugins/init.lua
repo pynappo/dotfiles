8c37423a36a6dbee23afc4094a965ec7ca8da251
pynappo <lehtien.david@gmail.com>
2022-10-28 19:42:14 -0700

    made packer a little nicer

--- .config/nvim/lua/pynappo/plugins/init.lua
+++ .config/nvim/lua/pynappo/plugins/init.lua
@@ -38,7 +38,7 @@ require('packer').startup({function(use)
     requires = 'nvim-treesitter/nvim-treesitter',
     config = function()
       local hl_list = {}
-      for i, color in pairs({ '#662121', '#767621', '#216631', '#325a5e', '#324b7b', '#562155' }) do
+      for i, color in pairs({ '#862121', '#6a6a21', '#216631', '#325f5f', '#324b7b', '#562155' }) do
         local name = 'IndentBlanklineIndent' .. i
         vim.api.nvim_set_hl(0, name, { fg = color })
         table.insert(hl_list, name);
@@ -297,9 +297,26 @@ require('packer').startup({function(use)
     },
     config = [[require("octo").setup()]]
   }
+  -- use {
+  --   'nvim-tree/nvim-tree.lua',
+  --   config = function()
+  --     require("nvim-tree").setup({
+  --       actions = {
+  --         change_dir = {
+  --           global = true
+  --         }
+  --       }
+  --     })
+  --   end
+  -- }
   if packer_bootstrap then
     require('packer').sync()
   end
 end,
-  config = { max_jobs = 12 }
+  config = {
+    max_jobs = 12,
+    display = {
+      prompt_border = 'single',
+    },
+  }
 })
