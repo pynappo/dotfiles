bf359e13c2fa9e32139dbc16a4e327804229d2f0
pynappo <lehtien.david@gmail.com>
2022-11-11 09:53:43 -0800

    nicer statusline

--- .config/nvim/lua/pynappo/plugins/lualine.lua
+++ .config/nvim/lua/pynappo/plugins/lualine.lua
@@ -1,9 +1,38 @@
-local function modified()
-  if vim.bo.modified then return '[+]' end
+local navic = require('nvim-navic')
+local gitsigns = {
+  'diff',
+  colored = true, -- Displays a colored diff status if set to true
+  diff_color = {
+    -- Same color values as the general color option can be used here.
+    added    = 'DiffAdd',    -- Changes the diff's added color
+    modified = 'DiffChange', -- Changes the diff's modified color
+    removed  = 'DiffDelete', -- Changes the diff's removed color you
+  },
+  symbols = {added = '+', modified = '~', removed = '-'}, -- Changes the symbols used by the diff.
+  source = 'b:gitsigns_status_dict',
+}
+
+local ls_icons = {
+  copilot = '',
+  ['null-ls'] = 'ﳠ'
+}
+local language_servers = function()
+  local status = ''
+  for _, v in ipairs(vim.lsp.get_active_clients()) do
+    if ls_icons[v.name] then
+      status = status .. ' ' .. ls_icons[v.name]
+    else
+      local devicons = require('nvim-web-devicons')
+      local icon, _ = devicons.get_icon_by_filetype(v.filetypes[1])
+      status = status .. ' ' .. icon
+    end
+  end
+  return status
 end
+
 require('lualine').setup {
   options = {
-    component_separators = { left = "|", right = "|" },
+    component_separators = { left = "", right = "" },
     section_separators = { left = '', right = '' },
     global_statusline = true,
     disabled_filetypes = {
@@ -14,73 +43,26 @@ require('lualine').setup {
     lualine_a = {
       { 'mode', separator = { left = '' }, right_padding = 2 },
     },
-    lualine_b = { 'filename', 'branch', 'diagnostics' },
-    lualine_c = {
-      -- { navic.get_location, cond = navic.is_available },
-      -- {
-      --   'lsp_progress',
-      --   separators = {
-      --     component = ' ',
-      --     progress = ' | ',
-      --     message = { pre = '(', post = ')' },
-      --     percentage = { pre = '', post = '%% ' },
-      --     title = { pre = '', post = ': ' },
-      --     lsp_client_name = { pre = '[', post = ']' },
-      --     spinner = { pre = '', post = '' },
-      --   },
-      --   display_components = { 'lsp_client_name', 'spinner', { 'title', 'percentage', 'message' } },
-      --   timer = { progress_enddelay = 500, spinner = 1000, lsp_client_name_enddelay = 1000 },
-      --   spinner_symbols = { '🌑 ', '🌒 ', '🌓 ', '🌔 ', '🌕 ', '🌖 ', '🌗 ', '🌘 ' },
-      --   message = { commenced = 'In Progress', completed = 'Completed' },
-      --   max_message_length = 30,
-      -- }
-    },
+    lualine_b = { 'branch' },
+    lualine_c = {{ navic.get_location, cond = navic.is_available }},
     lualine_x = { 'encoding', 'fileformat' },
-    lualine_y = { 'filetype' },
-    lualine_z = {
-      { 'progress', separator = { right = '' }, left_padding = 0 },
-    },
+    lualine_y = { language_servers },
+    lualine_z = { { 'progress', separator = { right = '' }, left_padding = 0 }, },
   },
   tabline = {},
   winbar = {
     lualine_a = {},
-    lualine_b = {
-      {
-        'diff',
-        colored = true, -- Displays a colored diff status if set to true
-        diff_color = {
-          -- Same color values as the general color option can be used here.
-          added    = 'DiffAdd',    -- Changes the diff's added color
-          modified = 'DiffChange', -- Changes the diff's modified color
-          removed  = 'DiffDelete', -- Changes the diff's removed color you
-        },
-        symbols = {added = '+', modified = '~', removed = '-'}, -- Changes the symbols used by the diff.
-        source = 'b:gitsigns_status_dict',
-      }
-    },
+    lualine_b = { gitsigns },
     lualine_c = {},
-    lualine_x = {{'%f', color = {gui = vim.bo.modified and 'italic' or ''}}},
-    lualine_y = {'progress'},
+    lualine_x = {},
+    lualine_y = {'diagnostics', 'progress'},
     lualine_z = {}
   },
   inactive_winbar = {
     lualine_a = {},
-    lualine_b = {
-      {
-        'diff',
-        colored = true, -- Displays a colored diff status if set to true
-        diff_color = {
-          -- Same color values as the general color option can be used here.
-          added    = 'DiffAdd',    -- Changes the diff's added color
-          modified = 'DiffChange', -- Changes the diff's modified color
-          removed  = 'DiffDelete', -- Changes the diff's removed color you
-        },
-        symbols = {added = '+', modified = '~', removed = '-'}, -- Changes the symbols used by the diff.
-        source = 'b:gitsigns_status_dict',
-      }
-    },
+    lualine_b = { gitsigns },
     lualine_c = {},
-    lualine_x = {{'%f', color = {gui = vim.bo.modified and 'italic' or ''}}},
+    lualine_x = {'%=', {'%f', color = {gui = vim.bo.modified and 'italic' or ''}}},
     lualine_y = {'progress'},
     lualine_z = {}
   },
