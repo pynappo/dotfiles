20215752ea2b58511a6c38697146a8535cdc274d
pynappo <lehtien.david@gmail.com>
2023-06-29 19:16:24 -0700

    Heirline revamp with WIP dropbar support

--- .config/nvim/lua/pynappo/plugins/heirline/components/base.lua
+++ .config/nvim/lua/pynappo/plugins/heirline/components/base.lua
@@ -1,7 +1,4 @@
 local conditions = require('heirline.conditions')
-local utils = require('heirline.utils')
-
-local space = { provider = ' ' }
 local M = {
   vi_mode = {
     init = function(self)
@@ -58,7 +55,7 @@ local M = {
   },
   cwd = {
     init = function(self)
-      self.icon = (vim.fn.haslocaldir(0) == 1 and '(Local)' or '') .. ' ' .. ' '
+      self.icon = (vim.fn.haslocaldir(0) == 1 and '(Local)' or '') .. '  '
       self.cwd = vim.fn.fnamemodify(vim.fn.getcwd(0), ':~')
     end,
     update = { 'DirChanged' },
@@ -130,105 +127,63 @@ local M = {
     },
     update = {'BufEnter', 'BufWritePost'}
   },
-  navic = {
-    flexible = 3,
-    {
-      condition = function()
-        return require('nvim-navic').is_available()
-      end,
-      static = {
-        -- create a type highlight map
-        type_hl = {
-          File = 'Directory',
-          Module = '@include',
-          Namespace = '@namespace',
-          Package = '@include',
-          Class = '@structure',
-          Method = '@method',
-          Property = '@property',
-          Field = '@field',
-          Constructor = '@constructor',
-          Enum = '@field',
-          Interface = '@type',
-          Function = '@function',
-          Variable = '@variable',
-          Constant = '@constant',
-          String = '@string',
-          Number = '@number',
-          Boolean = '@boolean',
-          Array = '@field',
-          Object = '@type',
-          Key = '@keyword',
-          Null = '@comment',
-          EnumMember = '@field',
-          Struct = '@structure',
-          Event = '@keyword',
-          Operator = '@operator',
-          TypeParameter = '@type',
-        },
-        -- bit operation dark magic, see below...
-        enc = function(line, col, winnr) return bit.bor(bit.lshift(line, 16), bit.lshift(col, 6), winnr) end,
-        -- line: 16 bit (65535); col: 10 bit (1023); winnr: 6 bit (63)
-        dec = function(c)
-          local line = bit.rshift(c, 16)
-          local col = bit.band(bit.rshift(c, 6), 1023)
-          local winnr = bit.band(c, 63)
-          return line, col, winnr
-        end,
-      },
-      init = function(self)
-        local data = require('nvim-navic').get_data() or {}
-        local children = {}
-        -- create a child for each level
-        for i, d in ipairs(data) do
-          -- encode line and column numbers into a single integer
-          local pos = self.enc(d.scope.start.line, d.scope.start.character, self.winnr)
-          local child = {
-            { provider = d.icon, hl = self.type_hl[d.type] },
-            {
-              provider = d.name:gsub('%%', '%%%%'):gsub('%s*->%s*', ''),
-              on_click = {
-                -- pass the encoded position through minwid
-                minwid = pos,
-                callback = function(_, minwid)
-                  local line, col, winnr = self.dec(minwid)
-                  vim.api.nvim_win_set_cursor(vim.fn.win_getid(winnr), { line, col })
-                end,
-                name = 'heirline_navic',
-              },
+  dropbar = {
+    condition = function() return not vim.tbl_isempty(dropbar) end,
+    init = function(self)
+      local data = dropbar.bars[vim.api.nvim_get_current_buf()][vim.api.nvim_get_current_win()]
+      local components = data.components
+      local children = {}
+      for i, c in ipairs(components) do
+        local child = {
+          {
+            provider = c.icon,
+            hl = c.icon_hl
+          },
+          {
+            provider = c.name,
+            hl = c.name_hl,
+            on_click = {
+              callback = function(_, ...)
+                c:on_click(...)
+              end,
+              name = "heirline_dropbar",
             },
-          }
-          if #data > 1 and i < #data then table.insert(child, { provider = ' > ', hl = { fg = 'normal' } }) end
-          table.insert(children, child)
+          },
+        }
+        if #components > 1 and i < #components then
+          local sep = data.separator
+          table.insert(child, {
+            provider = sep.icon,
+            hl = sep.icon_hl,
+            on_click = {
+              callback = function(_, ...)
+                c:on_click(...)
+              end,
+              name = "heirline_dropbar_separator",
+            }
+          })
         end
-        -- instantiate the new child, overwriting the previous one
-        self.child = self:new(children, 1)
-      end,
-      -- evaluate the children containing navic components
-      provider = function(self)
-        return self.child:eval()
-      end,
-      hl = { fg = 'normal' },
-    },
-    { provider = '' },
+        table.insert(children, child)
+      end
+      self.child = self:new(children, 1)
+    end,
+    provider = function(self) return self.child:eval() end,
   },
   diagnostics = {
     {
       condition = conditions.has_diagnostics,
       init = function(self)
-        local diag = vim.diagnostic
-        local diagnostics_count = {
-          error = #diag.get(0, { severity = diag.severity.ERROR }),
-          warning = #diag.get(0, { severity = diag.severity.WARN }),
-          hint = #diag.get(0, { severity = diag.severity.HINT }),
-          info = #diag.get(0, { severity = diag.severity.INFO }),
-        }
+        local buffer_diagnostics = vim.diagnostic.get(0, {severity = {min = vim.diagnostic.severity.INFO}})
+        local diagnostic_counts = {0,0,0,0}
+        for _, d in ipairs(buffer_diagnostics) do
+          diagnostic_counts[d.severity] = diagnostic_counts[d.severity] + 1
+        end
         local children = {}
-        for diag, count in pairs(diagnostics_count) do
+        for d, count in pairs(diagnostic_counts) do
           if count > 0 then
             table.insert(children, {
-              provider = string.format('%s%s', self.icons[diag], count),
-              hl = self.highlights[diag],
+              provider = self.icons[d] .. count,
+              hl = self.highlights[d],
             })
           end
         end
@@ -237,16 +192,16 @@ local M = {
       end,
       static = {
         icons = {
-          error = vim.fn.sign_getdefined('DiagnosticSignError')[1].text,
-          warning = vim.fn.sign_getdefined('DiagnosticSignWarn')[1].text,
-          info = vim.fn.sign_getdefined('DiagnosticSignInfo')[1].text,
-          hint = vim.fn.sign_getdefined('DiagnosticSignHint')[1].text,
+          vim.fn.sign_getdefined('DiagnosticSignError')[1].text,
+          vim.fn.sign_getdefined('DiagnosticSignWarn')[1].text,
+          vim.fn.sign_getdefined('DiagnosticSignInfo')[1].text,
+          vim.fn.sign_getdefined('DiagnosticSignHint')[1].text,
         },
         highlights = {
-          error = 'DiagnosticError',
-          warning = 'DiagnosticWarn',
-          info = 'DiagnosticInfo',
-          hint = 'DiagnosticHint',
+          'DiagnosticError',
+          'DiagnosticWarn',
+          'DiagnosticInfo',
+          'DiagnosticHint',
         },
       },
       on_click = {
@@ -255,11 +210,11 @@ local M = {
       },
       provider = function(self) return self.child:eval() end,
     },
-    update = 'DiagnosticChanged'
+    update = { 'DiagnosticChanged', 'BufEnter' }
   },
   dap = {
     {
-      condition = function() return require('dap').session() ~= nil end,
+      condition = function() return require('dap').session() end,
       provider = function() return ' ' .. require('dap').status() .. ' ' end,
       hl = 'Debug',
       {
@@ -323,22 +278,10 @@ local M = {
     provider = '%7(%l/%3L%):%2c - %P',
     update = { 'CursorMoved', 'ModeChanged' }
   },
-  scrollbar = {
-    static = { sbar = { '█', '▇', '▆', '▅', '▄', '▃', '▂', '▁' } },
-    provider = function(self)
-      local curr_line = vim.api.nvim_win_get_cursor(0)[1]
-      local lines = vim.api.nvim_buf_line_count(0)
-      local i = math.floor((curr_line - 1) / lines * #self.sbar) + 1
-      return string.rep(self.sbar[i], 2)
-    end,
-    update = 'CursorMoved',
-    hl = { fg = 'func', bg = 'bright_bg' },
-  },
   lsp_icons = {
     update = { 'LspAttach', 'LspDetach' },
     init = function(self)
       self.servers = vim.lsp.get_active_clients()
-      self.devicon_by_filetype = require('nvim-web-devicons').get_icon_by_filetype
     end,
     on_click = {
       callback = function()
@@ -352,15 +295,22 @@ local M = {
         ['null-ls'] = '󰟢',
         jdtls = '',
       },
+      devicon_by_filetype = require('nvim-web-devicons').get_icon_by_filetype
     },
     provider = function(self)
-      local icons = {}
       if #self.servers == 0 then return 'N/A' end
-      for _, s in ipairs(self.servers) do
-        table.insert(icons, self.ls_icons[s.name] or self.devicon_by_filetype(s.config.filetypes[1]) or '')
-      end
+      local icons = vim.tbl_map(function(server)
+        return self.ls_icons[server.name] or self.devicon_by_filetype(server.config.filetypes[1]) or ''
+      end, self.servers)
       return table.concat(icons, ' ')
     end,
+  },
+  lazy = {
+    condition = function()
+      local ok, lazy_status = pcall(require, "lazy.status")
+      return ok and lazy_status.has_updates()
+    end,
+    provider = function() return require("lazy.status").updates() end,
   },
   file_flags = {
     update = { 'BufWritePost', 'BufEnter', 'InsertEnter', 'TextChanged' },
@@ -402,8 +352,7 @@ M.file_info = {
   init = function(self)
     self.filename = vim.api.nvim_buf_get_name(0)
     self.extension = vim.fn.expand('%:e')
-    self.icon, self.icon_color =
-    require('nvim-web-devicons').get_icon_color(self.filename, self.extension, { default = true })
+    self.icon, self.icon_color = require('nvim-web-devicons').get_icon_color(self.filename, self.extension, { default = true })
   end,
   M.file_icon,
   M.filename,
--- .config/nvim/lua/pynappo/plugins/heirline/components/tabline.lua
+++ .config/nvim/lua/pynappo/plugins/heirline/components/tabline.lua
@@ -1,10 +1,10 @@
 local utils = require('heirline.utils')
-local M = {
-  tabline_bufnr = {
+local tabline = {
+  bufnr = {
     provider = function(self) return tostring(self.bufnr) .. '. ' end,
     hl = function(self) return { fg = 'comment', bold = self.is_visible } end,
   },
-  tabline_filename = {
+  filename = {
     init = function(self)
       self.lfilename = vim.fn.fnamemodify(self.filename, ':.')
       if self.lfilename == '' then self.lfilename = '[No Name]' end
@@ -12,7 +12,7 @@ local M = {
     hl = vim.bo.modified and { italic = true, force = true } or nil,
     provider = function(self) return vim.fn.pathshorten(self.lfilename) end,
   },
-  tabline_file_flags = {
+  file_flags = {
     {
       condition = function(self) return vim.bo[self.bufnr].modified end,
       provider = ' [+]',
@@ -24,7 +24,7 @@ local M = {
       hl = { fg = 'orange' },
     },
   },
-  tabline_close_button = {
+  close_button = {
     condition = function(self) return not vim.bo[self.bufnr].modified end,
     {
       provider = ' 󰅖',
@@ -36,7 +36,7 @@ local M = {
       },
     },
   },
-  tabline_picker = {
+  picker = {
     condition = function(self)
       return self._show_picker
     end,
@@ -59,15 +59,24 @@ local M = {
       return self.label
     end,
     hl = { fg = 'diag_warn', bold = true },
+  },
+  tabpage = {
+    init = function(self) self.name = vim.t[self.tabnr].name end,
+    provider = function(self) return '%' .. self.tabnr .. 'T ' .. self.tabnr .. (self.name and ' ' .. self.name or '') .. ' %T' end,
+    hl = function(self) return self.is_active and 'TabLineSel' or 'TabLine' end,
   }
 }
 
 local file_icon = require('pynappo/plugins/heirline/components/base').file_icon
-M.tabline_filename_block = {
+
+tabline.filename_block = {
   init = function(self)
     self.filename = vim.api.nvim_buf_get_name(self.bufnr)
-    self.icon, self.icon_color =
-    require('nvim-web-devicons').get_icon_color(self.filename, self.extension, { default = true })
+    self.icon, self.icon_color = require('nvim-web-devicons').get_icon_color(
+      self.filename,
+      self.extension,
+      { default = true }
+    )
   end,
   hl = function(self) return self.is_active and 'TabLineSel' or 'TabLine' end,
   on_click = {
@@ -81,21 +90,23 @@ M.tabline_filename_block = {
     minwid = function(self) return self.bufnr end,
     name = 'heirline_tabline_buffer_callback',
   },
-  M.tabline_bufnr,
+  tabline.bufnr,
   file_icon,
-  M.tabline_filename,
-  M.tabline_file_flags,
+  tabline.filename,
+  tabline.file_flags,
 }
-M.tabline_buffer_block = utils.surround(
+
+tabline.tabline_buffer_block = utils.surround(
   { '', '' },
   function(self) return self.is_active and 'tabline_sel' or 'tabline' end,
   {
-    M.tabline_filename_block,
-    M.tabline_close_button,
+    tabline.picker,
+    tabline.filename_block,
+    tabline.close_button,
   }
 )
 
-M.tabline_offset = {
+tabline.offset = {
   condition = function(self)
     local win = vim.api.nvim_tabpage_list_wins(0)[1]
     local bufnr = vim.api.nvim_win_get_buf(win)
@@ -114,23 +125,20 @@ M.tabline_offset = {
   end,
   hl = function(self) return vim.api.nvim_get_current_win() == self.winid and 'TablineSel' or 'Tabline' end,
 }
-M.bufferline = utils.make_buflist(
-  M.tabline_buffer_block,
+tabline.bufferline = utils.make_buflist(
+  tabline.tabline_buffer_block,
   { provider = '', hl = { fg = 'gray' } },
   { provider = '', hl = { fg = 'gray' } }
 )
-M.tabpages = {
+tabline.tabpages = {
   {
     condition = function() return #vim.api.nvim_list_tabpages() >= 2 end,
-    utils.make_tablist({
-      provider = function(self) return '%' .. self.tabnr .. 'T ' .. self.tabnr .. ' %T' end,
-      hl = function(self) return self.is_active and 'TabLineSel' or 'TabLine' end,
-    }),
+    utils.make_tablist(tabline.tabpage),
     {
       provider = '%999X 󰅖 %X',
       hl = 'TabLine',
     },
   },
-  update = {'TabEnter', 'TabLeave', 'TabNew', 'TabNewEntered', 'TabClosed'}
+  update = { 'TabEnter', 'TabLeave', 'TabNew', 'TabNewEntered', 'TabClosed', 'User' }
 }
-return M
+return tabline
