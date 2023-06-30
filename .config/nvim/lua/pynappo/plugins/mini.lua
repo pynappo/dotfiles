b61ed6c2fd506f1cd367e3d888cea475d6ca5a19
pynappo <lehtien.david@gmail.com>
2023-06-29 22:02:35 -0700

    update mini.nvim

--- .config/nvim/lua/pynappo/plugins/mini.lua
+++ .config/nvim/lua/pynappo/plugins/mini.lua
@@ -2,17 +2,42 @@ return {
   'echasnovski/mini.nvim',
   event = 'VeryLazy',
   config = function()
-    require('mini.pairs').setup()
+    -- require('mini.pairs').setup()
     require('mini.ai').setup()
     require('mini.move').setup()
     require('mini.bufremove').setup()
     require('mini.sessions').setup()
+    require('mini.align').setup({
+      mappings = {
+        start = '<leader>a',
+        start_with_preview = '<leader>A'
+      }
+    })
+    require('mini.surround').setup({
+      custom_surroundings = nil,
+      highlight_duration = 500,
+      mappings = require('pynappo.keymaps').mini.surround,
+      n_lines = 20,
+      respect_selection_type = true,
+      search_method = 'cover',
+
+      -- Whether to disable showing non-error feedback
+      silent = false,
+    })
+
+    vim.keymap.del('x', require('pynappo.keymaps').mini.surround.add)
     local indentscope = require('mini.indentscope')
     indentscope.setup({
       draw = {
-        animation = indentscope.gen_animation.quadratic({easing = 'out', duration = 12, unit = 'step'})
+        animation = indentscope.gen_animation.quadratic({easing = 'out', duration = 10, unit = 'step'})
       },
-      options = { try_as_border = true }
+      options = { try_as_border = true },
+      symbol = '󰇙'
+    })
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function(args)
+        if vim.tbl_contains({'nofile', 'help'}, vim.bo[args.buf].buftype) then vim.b[args.buf].miniindentscope_disable = true end
+      end
     })
     local trailspace = require('mini.trailspace')
     trailspace.setup()
@@ -22,8 +47,8 @@ return {
     end, {})
     require('pynappo/keymaps').setup.mini()
     local function set_mini_highlights()
-        vim.cmd.highlight('MiniIndentscopeSymbol guifg=#888888 gui=nocombine')
-        vim.cmd.highlight('MiniTrailspace guifg=#444444 gui=undercurl,nocombine')
+      vim.cmd.highlight('MiniIndentscopeSymbol guifg=#777777 gui=nocombine')
+      vim.cmd.highlight('MiniTrailspace guifg=#444444 gui=undercurl,nocombine')
     end
     set_mini_highlights()
     require('pynappo/autocmds').create('ColorScheme', { callback = set_mini_highlights })
