1e771f1f04de95d7b31c6ee8147d6436e5d3a642
pynappo <lehtien.david@gmail.com>
2023-07-24 04:44:49 -0700

    setup better colorscheme overriding system

--- .config/nvim/lua/pynappo/plugins/notifications.lua
+++ .config/nvim/lua/pynappo/plugins/notifications.lua
@@ -1,24 +1,25 @@
 return {
   {
     'rcarriga/nvim-notify',
-    config = function() require('notify').setup({ background_colour = '#000000' }) end,
-  },
-  {
-    'vigoux/notifier.nvim',
-    opts = {
-      {
-        ignore_messages = {}, -- Ignore message from LSP servers with this name
-        components = {  -- Order of the components to draw from top to bottom (first nvim notifications, then lsp)
-          -- "nvim",  -- Nvim notifications (vim.notify and such)
-          "lsp"  -- LSP status updates
-        },
-        notify = {
-          clear_time = 5000, -- Time in milliseconds before removing a vim.notify notification, 0 to make them sticky
-          min_level = vim.log.levels.INFO, -- Minimum log level to print the notification
-        },
-        component_name_recall = false, -- Whether to prefix the title of the notification by the component name
-        zindex = 50, -- The zindex to use for the floating window. Note that changing this value may cause visual bugs with other windows overlapping the notifier window.
+    config = function()
+      require('notify').setup({ background_colour = '#000000' })
+      local levels = {
+        ERROR = 'Error',
+        WARN = 'Warn',
+        INFO = 'Info',
+        DEBUG = 'Hint',
+        TRACE = 'Ok'
       }
-    }
+      local sections = {
+        Title = '',
+        Border = '',
+        Icon = 'Sign',
+      }
+      for notify_level, diagnostic_level in pairs(levels) do
+        for notify_section, diagnostic_section in pairs(sections) do
+          require('pynappo.theme').overrides.all_themes.links['Notify' .. notify_level .. notify_section] = 'Diagnostic' .. diagnostic_section .. diagnostic_level
+        end
+      end
+    end,
   },
 }
