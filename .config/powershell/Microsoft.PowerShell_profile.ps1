9e8f0183f30afe9d7b05b47bb8a5e331587d2761
pynappo <lehtien.david@gmail.com>
2023-07-24 04:56:42 -0700

    random tweaks

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -32,9 +32,9 @@ if (CanUsePredictionSource){
 $env:FZF_DEFAULT_COMMAND="fd --hidden --follow --exclude .git --color=always --strip-cwd-prefix"
 $env:FZF_CTRL_T_COMMAND=$env:FZF_DEFAULT_COMMAND
 $env:FZF_DEFAULT_OPTS = "--height=80% --layout=reverse --ansi --info=inline --tabstop=2 -m --cycle --scroll-off=4"
-$env:XDG_CONFIG_HOME = "$HOME/.config"
-$env:MPV_HOME = "$HOME/.config/mpv"
-$env:ANIMDL_CONFIG = "$HOME/.config/animdl/config.yml"
+$env:XDG_CONFIG_HOME = "$HOME\.config"
+$env:MPV_HOME = "$HOME\.config\mpv"
+$env:ANIMDL_CONFIG = "$HOME\.config\animdl\config.yml"
 Function fzfb { fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}' @Args }
 Set-Alias -Name f -Value fzfb
 $env:PYTHONIOENCODING="utf-8"
