f86290bbb763fb4a5b27ac32773f3c937132cdb7
pynappo <lehtien.david@gmail.com>
2023-07-30 00:54:54 -0700

    fix fish abbrs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    if set -q TERMUX_VERSION
+	    fish_add_path -p "$HOME/neovim/build/bin"
+    end
 end
 
+abbr -a -- dotfiles 'git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME'
+abbr -a -- df 'git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME'
