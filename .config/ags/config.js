2d24e746dc864d46c97026fa0dab45df5c41e4b3
pynappo <lehtien.david@gmail.com>
2023-12-30 02:48:51 -0800

    add ags and stuff

---
+++ .config/ags/config.js
@@ -0,0 +1,15 @@
+import Widget from 'resource:///com/github/Aylur/ags/widget.js'
+const myLabel = Widget.Label({
+  label: 'some example content',
+});
+
+const myBar = Widget.Window({
+  name: 'bar',
+  anchor: ['bottom', 'left', 'right'],
+  child: myLabel,
+});
+export default {
+  windows: [
+    myBar
+  ]
+};
