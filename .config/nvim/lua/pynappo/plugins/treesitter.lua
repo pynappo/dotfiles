cdae0df2baf85ed84b73335273e88c1c00ca9551
pynappo <lehtien.david@gmail.com>
2024-02-20 15:58:49 -0800

    add test mdx queries

--- .config/nvim/lua/pynappo/plugins/treesitter.lua
+++ .config/nvim/lua/pynappo/plugins/treesitter.lua
@@ -78,10 +78,22 @@ return {
           },
         },
       }, require('pynappo.keymaps').treesitter))
-      vim.treesitter.language.register('markdown', 'mdx')
+      -- vim.treesitter.language.register('markdown', 'mdx')
       vim.o.foldmethod = 'expr'
       vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
       vim.o.foldenable = false
+      local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+      parser_config.mdx = {
+        install_info = {
+          url = '~/code/tree-sitter-mdx', -- local path or git repo
+          files = { 'src/parser.c' }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+          -- optional entries:
+          branch = 'main', -- default branch in case of git repo if different from master
+          generate_requires_npm = true, -- if stand-alone parser without npm dependencies
+          requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+        },
+        -- filetype = 'zu', -- if filetype does not match the parser name
+      }
     end,
     dependencies = {
       {
---
+++ .config/nvim/queries/mdx/injections.scm
@@ -0,0 +1,2 @@
+((jsx_tag) @injection.content (#set! injection.language "jsx"))
+((markdown_block) @injection.content (#set! injection.language "markdown"))
