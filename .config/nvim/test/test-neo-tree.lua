109b04759289633a98f0de5aca4f7e6cf0d7fb3e
pynappo <pynappo@proton.me>
2025-03-05 11:05:27 -0800

    update git config

--- .config/nvim/test/test-neo-tree.lua
+++ .config/nvim/test/test-neo-tree.lua
@@ -44,6 +44,22 @@ require('lazy').setup({
         },
       },
       -- opts = {}
+      config = function()
+        require('neo-tree').setup({
+          event_handlers = {
+            {
+              event = 'after_render',
+              handler = function()
+                local state = require('neo-tree.sources.manager').get_state('filesystem')
+                if not require('neo-tree.sources.common.preview').is_active() then
+                  state.config = { use_float = false } -- or whatever your config is
+                  state.commands.toggle_preview(state)
+                end
+              end,
+            },
+          },
+        })
+      end,
     },
     --   {
     --     'folke/snacks.nvim',
