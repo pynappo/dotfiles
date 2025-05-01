5e8500c8bc542b6e23789d9a457dcbe866c872c1
pynappo <pynappo@proton.me>
2025-05-01 04:09:51 -0700

    remove clangd tweaks

--- .config/nvim/lsp/clangd.lua
+++ .config/nvim/lsp/clangd.lua
@@ -1,21 +1,5 @@
 ---@type vim.lsp.Config
 return {
-  root_markers = {
-    '.git',
-    'Makefile',
-    'configure.ac',
-    'configure.in',
-    'config.h.in',
-    'meson.build',
-    'meson_options.txt',
-    'build.ninja',
-    'compile_commands.json',
-    'compile_flags.json',
-    '.git',
-  },
-  capabilities = {
-    offsetEncoding = { 'utf-16' },
-  },
   cmd = {
     'clangd',
     '--background-index',
