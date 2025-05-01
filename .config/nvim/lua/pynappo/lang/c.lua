8e2c8e6b73c11eb20466468ddca77e62d59a07de
pynappo <pynappo@proton.me>
2025-05-01 04:24:18 -0700

    add c

---
+++ .config/nvim/lua/pynappo/lang/c.lua
@@ -0,0 +1,11 @@
+vim.lsp.config('clangd', {
+  cmd = {
+    'clangd',
+    '--background-index',
+    '--clang-tidy',
+    '--header-insertion=iwyu',
+    '--completion-style=detailed',
+    '--function-arg-placeholders',
+    '--fallback-style=llvm',
+  },
+})
