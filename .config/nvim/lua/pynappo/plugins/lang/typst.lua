729a87d262fb5e0d88f1f839da22d767b17a4405
pynappo <pynappo@proton.me>
2025-09-20 18:48:15 -0700

    disable typst-preview for windows

--- .config/nvim/lua/pynappo/plugins/lang/typst.lua
+++ .config/nvim/lua/pynappo/plugins/lang/typst.lua
@@ -2,6 +2,7 @@ return {
   {
     'yochem/typst-preview.nvim',
     branch = 'ftplugin',
+    enabled = not require('pynappo.utils').is_windows,
     config = function() require('typst-preview').setup() end,
   },
 }
